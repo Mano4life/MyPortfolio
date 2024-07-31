242f3ffaff254cbc7bc7aa9391e6de7f3a7f5a6c
Mano4life <momenhelmi8@gmail.com>
2024-07-31 21:31:41 +0300

    handled skills section with ngFor

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -1,12 +1,40 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-skills',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './skills.component.html',
   styleUrl: './skills.component.css'
 })
 export class SkillsComponent {
-
+  imgPath = 'assets/pattern-rings.svg';
+  skills = [
+    {
+      title: 'HTML',
+      experience: '2 Years Experience'
+    },
+    {
+      title: 'CSS',
+      experience: '2 Years Experience'
+    },
+    {
+      title: 'Javascript',
+      experience: '1 Year Experience'
+    },
+    {
+      title: 'Angular',
+      experience: '4 months Years Experience'
+    },
+    {
+      title: 'Git',
+      experience: '6 Months Experience'
+    },
+    {
+      title: 'PHP',
+      experience: '1 Year Experience'
+    }
+  ]
 }
